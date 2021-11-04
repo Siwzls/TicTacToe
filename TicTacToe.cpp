6a865a7250a29f32e009240cf6637dd6b0bb31c6
BlackJeff <x>
2021-11-05 01:40:03 +0200

    Project is created

---
+++ TicTacToe.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+std::string cell1 = " 1. ";
+std::string cell2 = " 2. ";
+std::string cell3 = " 3. ";
+std::string cell4 = " 4. ";
+std::string cell5 = " 5. ";
+std::string cell6 = " 6. ";
+std::string cell7 = " 7. ";
+std::string cell8 = " 8. ";
+std::string cell9 = " 9. ";
+
+int main()
+{
+    std::cout << cell1 << cell2 << cell3 << std::endl;
+    std::cout << cell4 << cell5 << cell6 << std::endl;
+    std::cout << cell7 << cell8 << cell9 << std::endl;
+}
